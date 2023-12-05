329edd30ea1d16586e186b505a25a656c108e5a0
awsbilal <142546269+awsbilal@users.noreply.github.com>
2023-12-05 15:36:19 -0600

    new changes December3

--- src/App.js
+++ src/App.js
@@ -14,85 +14,101 @@ const App = ({ signOut }) => {
   const [finalTime2Count, setFinalTime2Count] = useState(null);
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
-async function fetchData() {
-  const apiData = await API.graphql({ query: listDemoprojtables });
-  setDemoprojData(apiData.data.listDemoprojtables.items);
-}
-
-const SixtyTrue = async () => {
-  const input = {
-    id: '1',
-    sixty: true,
-    status: '3'
-  };
-  return API.graphql(graphqlOperation(updateDemoprojtable, {input}));
-}
-
-const SixtyFalse = async () => {
-  const input = {
-    id: '1',
-    sixty: false
-  };
-  return API.graphql(graphqlOperation(updateDemoprojtable, {input}));
-}
-
-useEffect(() => {
-  let interval;
-  if (isTimerRunning) {
-    interval = setInterval(() => {
-      setTime(prev => prev + 1); 
-      setTime2(prev => prev + 1);
-    }, 1000);
+  // Fetch data from the API
+  async function fetchData() {
+    const apiData = await API.graphql({ query: listDemoprojtables });
+    setDemoprojData(apiData.data.listDemoprojtables.items);
   }
-  return () => clearInterval(interval); 
-}, [isTimerRunning]);
 
-useEffect(() => {
-  const interval = setInterval(() => {
-    fetchData(); 
-  }, 5000);
-
-return () => clearInterval(interval);
-}, []) 
-
-useEffect(() => {
-  if (!isTimerRunning && time2 !== 12.38) {
-    setFinalTime2Count(time2);
+  // Function to handle 60 seconds condition
+  const SixtyTrue = async () => {
+    const input = {
+      id: '1',
+      sixty: true,
+      status: '3'
+    };
+    return API.graphql(graphqlOperation(updateDemoprojtable, {input}));
   }
-}, [isTimerRunning, time2]);
 
-useEffect(() => {
-  const latestItem = demoprojData[demoprojData.length - 1];
-  if(latestItem?.time === 'start') {
-    setIsTimerRunning(true);
-  } else if (latestItem?.time === 'stop') {
-    const lastTime = time2;
-    setIsTimerRunning(false);
+  const SixtyFalse = async () => {
+    const input = {
+      id: '1',
+      sixty: false
+    };
+    return API.graphql(graphqlOperation(updateDemoprojtable, {input}));
   }
-}, [demoprojData]);
 
-useEffect(() => {
-  if(time >= 60) {
-    SixtyTrue();
+  // Timer effect
+  useEffect(() => {
+    let interval;
+    if (isTimerRunning) {
+      interval = setInterval(() => {
+        setTime(prev => prev + 1); 
+        setTime2(prev => prev + 1);
+      }, 1000);
+    } else if (finalTime2Count === null) {
+      setFinalTime2Count(time2);
+    }
+    return () => clearInterval(interval); 
+  }, [isTimerRunning, time2, finalTime2Count]);
 
-  } else{
-    SixtyFalse(); 
+  // Fetch data periodically
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchData(); 
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []) 
+
+  // Effect for checking the latest item in the data
+  useEffect(() => {
+    const latestItem = demoprojData[demoprojData.length - 1];
+    if(latestItem?.time === 'start') {
+      setIsTimerRunning(true);
+      setFinalTime2Count(null); // Reset finalTime2Count on restart
+    } else if (latestItem?.time === 'stop') {
+      setIsTimerRunning(false);
+      setFinalTime2Count(time2); // Store the last time2 value
+    }
+  }, [demoprojData, time2]);
+
+  // Effect for handling 60 seconds condition
+  useEffect(() => {
+    if(time >= 60) {
+      SixtyTrue();
+    } else {
+      SixtyFalse(); 
+    }
+  }, [time]);
+
+  // Function to determine the status color
+  function getStatusColor(item) {
+    switch (item.status) {
+      case 1:
+        return 'green';
+      case 2:
+        return 'yellow';
+      case 3:
+        return 'red';
+      default:
+        return 'grey';
+    }
   }
-}, [time]);
 
-function getStatusColor(item) {
-  if(item.status === 1) {
-    return 'green';
-  } else if (item.status === 2) {
-   return 'yellow';
-  } else if (item.status === 3) {
-   return 'red'; 
-  }
-  return 'grey';
-}
+  // Format date and time
   const formatDateTime = (date) => {
     return date.toLocaleString('en-US', { hour12: true });
   };
+
+  // Update the current date and time every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <View className="App">
@@ -106,14 +122,12 @@ function getStatusColor(item) {
           <Button onClick={signOut} marginTop="auto">Sign Out</Button>
         </Flex>
 
-        {/* Main Content */}
         <Flex direction="column" width="80%" padding="1rem">
-          <Heading level={1} fon>ACME Inc.</Heading>
+          <Heading level={1}>ACME Inc.</Heading>
           <Text>Distribution Center: San Bernardino, CA</Text>
 
-          {/* Lane Headers */}
           <Flex direction="row" justifyContent="center" backgroundColor="#2e73b8">
-          <Text width="16.66%" padding="0.5rem" fontWeight="bold" color={"white"}>Lane Description</Text>
+            <Text width="16.66%" padding="0.5rem" fontWeight="bold" color={"white"}>Lane Description</Text>
             <Text width="16.66%" padding="0.5rem" fontWeight="bold" color={"white"}>Lane Status</Text>
             <Text width="16.66%" padding="0.5rem" fontWeight="bold" color={"white"}>Initial SMS</Text>
             <Text width="16.66%" padding="0.5rem" fontWeight="bold" color={"white"}>Escalation SMS</Text>
@@ -122,22 +136,19 @@ function getStatusColor(item) {
             <Text width="16.66%" padding="0.5rem" fontWeight="bold" color={"white"}>Final Losses</Text>
           </Flex>
 
-          {/* Dynamically generated lane information */}
-<Flex direction="column" width="80%" padding="1rem">
-  {demoprojData.map((item, index) => (
-    <Flex key={index} direction="row" justifyContent="center" backgroundColor={getStatusColor(item)}>
-      <Text width="16.66%" padding="0.5rem">{`Take-away #${item.id} Pallet Build`}</Text>
-      <Text width="16.66%" padding="0.5rem">{item.laneStatus}</Text>
-      <Text width="16.66%" padding="0.5rem">{item.initialSMS}</Text>
-      <Text width="16.66%" padding="0.5rem">{item.escalationSMS}</Text>
-      <Text width="16.66%" padding="0.5rem">{item.responseTime}</Text>
-      <Text width="16.66%" padding="0.5rem">{item.laneLosses}</Text>
-      <Text width="16.66%" padding="0.5rem">{item.finalLosses}</Text>
-    </Flex>
-  ))}
-</Flex>
+          <Flex direction="column" width="80%" padding="1rem">
+            {demoprojData.map((item, index) => (
+              <Flex key={index} direction="row" justifyContent="center" backgroundColor={getStatusColor(item)}>
+                <Text width="16.66%" padding="0.5rem">{`Take-away #${item.id} Pallet Build`}</Text>
+                <Text width="16.66%" padding="0.5rem">{item.laneStatus}</Text>
+                <Text width="16.66%" padding="0.5rem">{item.initialSMS}</Text>
+                <Text width="16.66%" padding="0.5rem">{item.escalationSMS}</Text>
+                <Text width="16.66%" padding="0.5rem">{isTimerRunning ? time2 : item.laneLosses}</Text>
+                <Text width="16.66%" padding="0.5rem">{finalTime2Count !== null ? finalTime2Count : item.finalLosses}</Text>
+              </Flex>
+            ))}
+          </Flex>
 
-          {/* Current date and time display */}
           <Flex justifyContent="center" backgroundColor="white">
             <Text padding="0.5rem">{formatDateTime(currentDateTime)}</Text>
           </Flex>
