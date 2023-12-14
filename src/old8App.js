d78437b08d07c4aebc519de68aaa5ce07a824179
awsbilal <142546269+awsbilal@users.noreply.github.com>
2023-12-14 15:02:10 -0600

    new changes December33

--- src/old8App.js
+++ src/old8App.js
@@ -125,7 +125,7 @@ const App = ({ signOut }) => {
               </div>
               <div className="grid-cell">{item.status ? time : 'N/A'}</div>
               <div className="grid-cell">{isTimerRunning ? 'Sent' : 'Not Sent'}</div>
-              <div className="grid-cell">{time >= 60 ? 'Sent' : 'Not Sent'}</div>
+              <div className="grid-cell">{time === 60 ? 'Sent' : 'Not Sent'}</div>
               <div className="grid-cell">{lastTime}</div>
               <div className="grid-cell">{laneLosses.toFixed(2)}</div>
             </React.Fragment>
